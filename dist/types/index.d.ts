36c7702df135882849f85f27ad6fcd84b2c910be
Роман Балабаев <ice187boy@gmail.com>
2023-10-20 14:47:57 +0300

    hard fix

---
+++ dist/types/index.d.ts
@@ -0,0 +1,6 @@
+
+console.log(7777777777);
+import AudioPlayer from './components/AudioPlayer.vue'; 
+import { AudioPlayerOption } from './components/types';
+export default AudioPlayer;
+export type { AudioPlayerOption };
